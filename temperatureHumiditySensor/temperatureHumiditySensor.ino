e7ca50da58a7030559c78934b365dccfd0cf3955
人菜瘾大 <ccxiaoy@hotmail.com>
2023-04-26 04:18:11 +0800

    feat: modify the humid temp sensor to satisfy front-end's need

--- temperatureHumiditySensor/temperatureHumiditySensor.ino
+++ temperatureHumiditySensor/temperatureHumiditySensor.ino
@@ -28,6 +28,9 @@ int count;  //  variable that used in Ticker count time(sec)
 float humidity = 0.0;
 float temperature = 0.0;
 
+int humidSensorSwitch = 1;  // 1 => open | 0 => close
+int temperatureSensorSwitch = 1;  // 1 => open | 0 => close
+
 void setup() {
   Serial.begin(9600);
 
@@ -42,7 +45,7 @@ void setup() {
 
   // Set MQTT server and port
   mqttClient.setServer(mqttServer, mqttPort);
-  // mqttClient.setCallback(receiveCallback);
+  mqttClient.setCallback(receiveCallback);
 
   // Connect MQTT Server
   connectMQTTServer();
@@ -99,6 +102,31 @@ void tickerCount() {
   count++;
 }
 
+// open humid sensor, 1 => open
+void openHumidSensor() {
+  humidSensorSwitch = 1;
+  pubMQTTmsg();
+}
+
+// close humid sensor, 0 => close
+void closeHumidSensor() {
+  humidSensorSwitch = 0;
+  pubMQTTmsg();
+}
+
+// open temperature sensor, 1 => open
+void openTemperatureSensor() {
+  temperatureSensorSwitch = 1;
+  pubMQTTmsg();
+}
+
+// close temperature sensor, 0 => close
+void closeTemperatureSensor() {
+  temperatureSensorSwitch = 0;
+  pubMQTTmsg();
+}
+
+
 // ESP8266 connect MQTT server
 void connectMQTTServer() {
   // according to ESP8266's mac address to generate client device ID (to avoid ID collision)
@@ -113,7 +141,7 @@ void connectMQTTServer() {
     Serial.println(mqttServer);
     Serial.println("ClientId:");
     Serial.println(clientId);
-    // subscribeTopic(); // subscribe to specific topic
+    subscribeTopic(); // subscribe to specific topic
   } else {
     Serial.print("MQTT Server Connect Failed. Client State:");
     Serial.println(mqttClient.state());
@@ -121,26 +149,21 @@ void connectMQTTServer() {
   }
 }
 
-// Publish messages
-void pubMQTTmsg() {
+// Publish humid sensor status (On / Off)
+void pubHumidSensorStatus() {
   // according to ESP8266's mac address to generate client device ID
   // (to avoid ID collision) when different users publish messages
-  String topicString = "humidTemperatureSensor-status-" + WiFi.macAddress();
+  String topicString = "humidSensor-status-" + WiFi.macAddress();
   char publishTopic[topicString.length() + 1];
   strcpy(publishTopic, topicString.c_str());
 
-  // Get sensor information
-  refreshState();
-  // Get humidity data
-  getHumidity();
-  // Get humidity data
-  getTemperature();
-
-  // publish the state of the autoFan
+  // publish the state of the autoLight
   String messageString;
-  // messageString = "{" + "\"temperature: " + String(temperature) + "," + "\"humidity: " + String(humidity) + "}";
-  messageString = "{ \"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "} ";
-  // messageString = String(shine()) + " degree";
+  if (humidSensorSwitch == 1) {
+    messageString = "On";
+  } else {
+    messageString = "Off";
+  }
   char publishMsg[messageString.length() + 1];
   strcpy(publishMsg, messageString.c_str());
 
@@ -152,6 +175,253 @@ void pubMQTTmsg() {
     Serial.println(publishMsg);
   } else {
     Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish humid sensor connection status (Online / Offline)
+void pubHumidSensorConnectionStatus() {
+  // according to ESP8266's mac address to generate client device ID
+  // (to avoid ID collision) when different users publish messages
+  String topicString = "humidSensor-connection-status-" + WiFi.macAddress();
+  char publishTopic[topicString.length() + 1];
+  strcpy(publishTopic, topicString.c_str());
+
+  // publish the connection state of the bright sensor
+  String messageString = "Online";
+
+  char publishMsg[messageString.length() + 1];
+  strcpy(publishMsg, messageString.c_str());
+
+  // achieve to publish message to MQTT server through ESP8266
+  if (mqttClient.publish(publishTopic, publishMsg)) {
+    Serial.println("Publish Topic:");
+    Serial.println(publishTopic);
+    Serial.println("Publish message:");
+    Serial.println(publishMsg);
+  } else {
+    Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish humid sensor power Degree
+void pubHumidDegree() {
+  // according to ESP8266's mac address to generate client device ID
+  // (to avoid ID collision) when different users publish messages
+  String topicString = "humidSensor-power-status-" + WiFi.macAddress();
+  char publishTopic[topicString.length() + 1];
+  strcpy(publishTopic, topicString.c_str());
+
+  // publish the state of the autoFan
+  String messageString;
+  messageString = String(humidity);
+  char publishMsg[messageString.length() + 1];
+  strcpy(publishMsg, messageString.c_str());
+
+  // achieve to publish message to MQTT server through ESP8266
+  if (mqttClient.publish(publishTopic, publishMsg)) {
+    Serial.println("Publish Topic:");
+    Serial.println(publishTopic);
+    Serial.println("Publish message:");
+    Serial.println(publishMsg);
+  } else {
+    Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish temperature sensor status (On / Off)
+void pubTemperatureSensorStatus() {
+  // according to ESP8266's mac address to generate client device ID
+  // (to avoid ID collision) when different users publish messages
+  String topicString = "tempSensor-status-" + WiFi.macAddress();
+  char publishTopic[topicString.length() + 1];
+  strcpy(publishTopic, topicString.c_str());
+
+  // publish the state of the autoLight
+  String messageString;
+  if (temperatureSensorSwitch == 1) {
+    messageString = "On";
+  } else {
+    messageString = "Off";
+  }
+  char publishMsg[messageString.length() + 1];
+  strcpy(publishMsg, messageString.c_str());
+
+  // achieve to publish message to MQTT server through ESP8266
+  if (mqttClient.publish(publishTopic, publishMsg)) {
+    Serial.println("Publish Topic:");
+    Serial.println(publishTopic);
+    Serial.println("Publish message:");
+    Serial.println(publishMsg);
+  } else {
+    Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish temperature sensor connection status (Online / Offline)
+void pubTemperatureSensorConnectionStatus() {
+  // according to ESP8266's mac address to generate client device ID
+  // (to avoid ID collision) when different users publish messages
+  String topicString = "tempSensor-connection-status-" + WiFi.macAddress();
+  char publishTopic[topicString.length() + 1];
+  strcpy(publishTopic, topicString.c_str());
+
+  // publish the connection state of the bright sensor
+  String messageString = "Online";
+
+  char publishMsg[messageString.length() + 1];
+  strcpy(publishMsg, messageString.c_str());
+
+  // achieve to publish message to MQTT server through ESP8266
+  if (mqttClient.publish(publishTopic, publishMsg)) {
+    Serial.println("Publish Topic:");
+    Serial.println(publishTopic);
+    Serial.println("Publish message:");
+    Serial.println(publishMsg);
+  } else {
+    Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish temperature sensor power Degree
+void pubTemperatureDegree() {
+  // according to ESP8266's mac address to generate client device ID
+  // (to avoid ID collision) when different users publish messages
+  String topicString = "tempSensor-power-status-" + WiFi.macAddress();
+  char publishTopic[topicString.length() + 1];
+  strcpy(publishTopic, topicString.c_str());
+
+  // publish the state of the autoFan
+  String messageString;
+  messageString = String(temperature);
+  char publishMsg[messageString.length() + 1];
+  strcpy(publishMsg, messageString.c_str());
+
+  // achieve to publish message to MQTT server through ESP8266
+  if (mqttClient.publish(publishTopic, publishMsg)) {
+    Serial.println("Publish Topic:");
+    Serial.println(publishTopic);
+    Serial.println("Publish message:");
+    Serial.println(publishMsg);
+  } else {
+    Serial.println("Message Publish Failed.");
+  }
+}
+
+// Publish messages
+void pubMQTTmsg() {
+  pubHumidSensorStatus();
+  pubHumidSensorConnectionStatus();
+  pubTemperatureSensorStatus();
+  pubTemperatureSensorConnectionStatus();
+  
+  // Get sensor information
+  refreshState();
+  // Get humidity data
+  getHumidity();
+  // Get humidity data
+  getTemperature();
+  
+  if(humidSensorSwitch == 1) {
+    pubHumidDegree();
+  }
+  if(temperatureSensorSwitch == 1) {
+    pubTemperatureDegree();
+  }
+}
+
+// subscribe humid sensor
+void subscribeHumidSensorTopic() {
+  // Create subscrib topic with postfix with Mac address
+  // to avoid topic collision when similar devices connect to the same devices
+  String topicString = "humidSensor-" + WiFi.macAddress();
+  char subTopic[topicString.length() + 1];  // plus one is a settled operation, you can see as the string has a hide one at the end
+  strcpy(subTopic, topicString.c_str());
+
+  // Through the serial monitor to check if subscribe the topic successfully and the name info about this topic
+  // method subscribe beneath require arguments to be a char array
+  if (mqttClient.subscribe(subTopic)) {
+    Serial.println("Subscribe Topic:");
+    Serial.println(subTopic);
+  } else {
+    Serial.print("Subscribe Fail...");
+  }  
+}
+
+// subscribe temperature sensor
+void subscribeTemperatureSensorTopic() {
+  // Create subscrib topic with postfix with Mac address
+  // to avoid topic collision when similar devices connect to the same devices
+  String topicString = "tempSensor-" + WiFi.macAddress();
+  char subTopic[topicString.length() + 1];  // plus one is a settled operation, you can see as the string has a hide one at the end
+  strcpy(subTopic, topicString.c_str());
+
+  // Through the serial monitor to check if subscribe the topic successfully and the name info about this topic
+  // method subscribe beneath require arguments to be a char array
+  if (mqttClient.subscribe(subTopic)) {
+    Serial.println("Subscribe Topic:");
+    Serial.println(subTopic);
+  } else {
+    Serial.print("Subscribe Fail...");
+  }  
+}
+
+// Subscrib specific topic
+// 后续改进 能不能通过外部传参 topic的前缀来制定订阅的topic名字
+void subscribeTopic() {
+  subscribeHumidSensorTopic();
+  subscribeTemperatureSensorTopic();  
+}
+
+// humid sensor callback
+void brightSensorCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message Received [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+  Serial.print("Message Length(Bytes) ");
+  Serial.println(length);
+
+  if ((char)payload[0] == '1') {  // if the message received started with 1
+    openHumidSensor();                    // Then sensor is opened.
+  } else {
+    closeHumidSensor();  // Otherwise the sensor is closed.
+  }
+}
+
+// temperature sensor callback
+void temperatureSensorCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message Received [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+  Serial.print("Message Length(Bytes) ");
+  Serial.println(length);
+
+  if ((char)payload[0] == '1') {  // if the message received started with 1
+    openTemperatureSensor();                    // Then sensor is opened.
+  } else {
+    closeTemperatureSensor();  // Otherwise the sensor is closed.
+  }
+}
+
+// Callback after receive message from MQTT server
+void receiveCallback(char* topic, byte* payload, unsigned int length) {
+  String comingTopic = topic;
+  String humidSensorTopicString = "humidSensor-" + WiFi.macAddress();
+  String tempSensorTopicString = "tempSensor-" + WiFi.macAddress();
+  // autolight callback
+  if (comingTopic == humidSensorTopicString) {
+    brightSensorCallback(topic, payload, length);
+  }
+  // lightSensor callback
+  if (comingTopic == tempSensorTopicString) {
+    temperatureSensorCallback(topic, payload, length);
   }
 }
 
@@ -217,44 +487,44 @@ double Kelvin(double celsius) {
 
 // 露点（点在此温度时，空气饱和并产生露珠）
 // 参考: http://wahiduddin.net/calc/density_algorithms.htm
-double dewPoint(double celsius, double humidity) {
-  double A0 = 373.15 / (273.15 + celsius);
-  double SUM = -7.90298 * (A0 - 1);
-  SUM += 5.02808 * log10(A0);
-  SUM += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / A0))) - 1);
-  SUM += 8.1328e-3 * (pow(10, (-3.49149 * (A0 - 1))) - 1);
-  SUM += log10(1013.246);
-  double VP = pow(10, SUM - 3) * humidity;
-  double T = log(VP / 0.61078);  // temp var
-  return (241.88 * T) / (17.558 - T);
-}
+// double dewPoint(double celsius, double humidity) {
+//   double A0 = 373.15 / (273.15 + celsius);
+//   double SUM = -7.90298 * (A0 - 1);
+//   SUM += 5.02808 * log10(A0);
+//   SUM += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / A0))) - 1);
+//   SUM += 8.1328e-3 * (pow(10, (-3.49149 * (A0 - 1))) - 1);
+//   SUM += log10(1013.246);
+//   double VP = pow(10, SUM - 3) * humidity;
+//   double T = log(VP / 0.61078);  // temp var
+//   return (241.88 * T) / (17.558 - T);
+// }
 
 // 快速计算露点，速度是5倍dewPoint()
 // 参考: http://en.wikipedia.org/wiki/Dew_point
-double dewPointFast(double celsius, double humidity) {
-  double a = 17.271;
-  double b = 237.7;
-  double temp = (a * celsius) / (b + celsius) + log(humidity / 100);
-  double Td = (b * temp) / (a - temp);
-  return Td;
-}
+// double dewPointFast(double celsius, double humidity) {
+//   double a = 17.271;
+//   double b = 237.7;
+//   double temp = (a * celsius) / (b + celsius) + log(humidity / 100);
+//   double Td = (b * temp) / (a - temp);
+//   return Td;
+// }
 
-void printFullState() {
-  Serial.print("Humidity (%): ");
-  Serial.println((float)DHT11.humidity, 2);
+// void printFullState() {
+//   Serial.print("Humidity (%): ");
+//   Serial.println((float)DHT11.humidity, 2);
 
-  Serial.print("Temperature (oC): ");
-  Serial.println((float)DHT11.temperature, 2);
+//   Serial.print("Temperature (oC): ");
+//   Serial.println((float)DHT11.temperature, 2);
 
-  Serial.print("Temperature (oF): ");
-  Serial.println(Fahrenheit(DHT11.temperature), 2);
+//   Serial.print("Temperature (oF): ");
+//   Serial.println(Fahrenheit(DHT11.temperature), 2);
 
-  Serial.print("Temperature (K): ");
-  Serial.println(Kelvin(DHT11.temperature), 2);
+//   Serial.print("Temperature (K): ");
+//   Serial.println(Kelvin(DHT11.temperature), 2);
 
-  Serial.print("Dew Point (oC): ");
-  Serial.println(dewPoint(DHT11.temperature, DHT11.humidity));
+//   Serial.print("Dew Point (oC): ");
+//   Serial.println(dewPoint(DHT11.temperature, DHT11.humidity));
 
-  Serial.print("Dew PointFast (oC): ");
-  Serial.println(dewPointFast(DHT11.temperature, DHT11.humidity));
-}
+//   Serial.print("Dew PointFast (oC): ");
+//   Serial.println(dewPointFast(DHT11.temperature, DHT11.humidity));
+// }
